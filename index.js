02a3bc2f7c0f45c98b4181093238292e15291479
dsanderson90 <dsanderson3@cnm.edu>
2019-08-07 01:58:52 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let spans = document.querySelectorAll(".ltr")
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+setInterval( () => {
+  spans.forEach(span => span.style.color = getRandomColor(), 100000);
+})
+
+
+
+
